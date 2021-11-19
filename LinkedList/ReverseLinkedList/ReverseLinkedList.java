28a7f5fae9b1b34bb844a028afcda26e1c7644a0
PhillipNinan <ninan.phillip@gmail.com>
2021-11-19 01:10:46 -0500

    cleanup

--- LinkedList/ReverseLinkedList/ReverseLinkedList.java
+++ LinkedList/ReverseLinkedList/ReverseLinkedList.java
@@ -19,19 +19,29 @@ class ListNode {
  */
 public class ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
+
+        // stacks are first in last out
+        // so the last one put onto the stack
+        // will be the first one popped off
         Stack<Integer> stack = new Stack<Integer>();
         
+        // put all the values onto the stack
         while(head!=null){
             stack.push(head.val);
             head = head.next;
         }
+
+        // create a base node
         ListNode out = new ListNode(-1);
+
         ListNode n = out;
         while(!stack.isEmpty()){
             Integer v = stack.pop();
             n.next = new ListNode(v);
+            // we need the links to be pointed in backwards order
             n = n.next;
         }
+        // we dont want to return the -1 we instantiated
         return out.next;
     }
 }
