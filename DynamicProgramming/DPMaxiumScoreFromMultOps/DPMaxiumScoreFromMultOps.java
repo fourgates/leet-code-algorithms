c39f5212d68f905df8fcc313c41e72abf568be55
phillip.ninan <pninan@lbisoftware.com>
2021-12-21 17:54:48 -0500

    working... but needs memory improvements

--- DynamicProgramming/DPMaxiumScoreFromMultOps/DPMaxiumScoreFromMultOps.java
+++ DynamicProgramming/DPMaxiumScoreFromMultOps/DPMaxiumScoreFromMultOps.java
@@ -24,21 +24,52 @@ public class DPMaxiumScoreFromMultOps {
 //
 // 1. DS or Function
 //
-// - we can use an array or hashmap...
-// the state is made up of a number for nums and a number from multipliers dp(i,j)
+// since we have multiple states a nth-D array may be worthwhile exploring...
 //
 // 2. Recurrence relation
 //
+//
 // - i = a multiplier
-// - j = subset of nums
-// - dp(i,j[]) = Math.max(dp(i, j[0]), dp(i,j[j.length - 1]))
+// - j = left most index
+// - k = right more index
+// - dp(i, j, k) = multiplier[i] * Math.max(nums[j], nums[k]) + 
+//                  Math.max(dp(i+1, j + 1, k), dp(i+1,j, k -1))
 //
 // 3. Base Cases
 //
-// - j.length == 1 - only one number!
-// - j.length == 0 - no nums
+//  - j = k then just return i * j
 class Solution {
+    private int[][][] memo;
+    private int[] nums;
+    private int[] multipliers;
     public int maximumScore(int[] nums, int[] multipliers) {
-        return -1;
+        if(nums.length == 1 && multipliers.length == 1){
+            return nums[0] * multipliers[0];
+        }
+        this.nums = nums;
+        this.multipliers = multipliers;
+        int m = nums.length;
+        this.memo = new int[m][m][m];
+        return dp(0, 0, nums.length - 1);
+    }
+    private int dp(int i, int j, int k){
+        if(i >= this.multipliers.length){
+            return 0;
+        }
+        if(memo[i][j][k] == 0){
+            int left = this.nums[j];
+            int right = this.nums[k];
+            memo[i][j][k] = Math.max(
+                // move left pointer right
+                (
+                    this.multipliers[i] * left + dp(i+1, j+1, k)
+                ),
+                // move right pointer left
+                (
+                    this.multipliers[i] * right + dp(i+1,j, k-1)
+                )
+            );
+        }
+        return memo[i][j][k];
     }
 }
