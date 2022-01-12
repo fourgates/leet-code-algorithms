c0b35649bd340763c7a6b4be6229653541353c30
phillip.ninan <pninan@lbisoftware.com>
2022-01-12 17:50:30 -0500

    update fully optimized UF and added some more documentation to remembe whats going on!

--- Graphs/DisjointSets/FullOptimizedDisjointSet.java
+++ Graphs/DisjointSets/FullOptimizedDisjointSet.java
@@ -2,6 +2,8 @@ package Graphs.DisjointSets;
 
 public class FullOptimizedDisjointSet {
     private int[] root; // data
+    
+    // Use a rank array to record the height of each vertex, i.e., the "rank" of each vertex.
     private int[] rank; // weighted ranks
 
     public FullOptimizedDisjointSet(int size){
@@ -10,9 +12,13 @@ public class FullOptimizedDisjointSet {
         for(int i=0;i<size;i++){
             root[i] = i;
             rank[0] = 1; // each rank is init to 1 since it points to itself
+
+            // The initial "rank" of each vertex is 1, because each of them is
+            // a standalone vertex with no connection to other vertices.
         }
     }
 
+	// The find function here is the same as that in the disjoint set with path compression.
     public int find(int x){
         if(x == root[x]){ // root
             return x;
@@ -20,14 +26,15 @@ public class FullOptimizedDisjointSet {
         return root[x] = find(root[x]);
     }
 
+    // The union function with union by rank
     public void union(int x, int y){
         int rootX = find(x);
         int rootY = find(y);
 
-        if(rootX > rootY){
+        if(rank[rootX] > rank[rootY]){
             root[rootY] = rootX;
         }
-        else if(rootY > rootX){
+        else if(rank[rootY] > rank[rootX]){
             root[rootX] = rootX;
         }
         else{
