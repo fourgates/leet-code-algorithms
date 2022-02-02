d314e14863c92a8554df70a9ae546581d6bf2558
PhillipNinan <ninan.phillip@gmail.com>
2022-02-01 22:13:30 -0500

    more details

--- LinkedList/MergeTwoLinkedLists/MergeIterator.java
+++ LinkedList/MergeTwoLinkedLists/MergeIterator.java
@@ -2,22 +2,86 @@ package LinkedList.MergeTwoLinkedLists;
 
 public class MergeIterator {
         
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {        
-        ListNode prehead = new ListNode(-1);
-        
-        ListNode prev = prehead;
-        while(l1 != null && l2 != null){
-            if(l1.val < l2.val){
-                prev.next = l1;
-                l1 = l1.next;
+
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// pseduo 1
+//
+//  if l1 is null or l2 is null
+//      return l1 or l2, whichever is not null
+//  if l1's value is less than l2
+//      set l1's next to mergeList while incrementing l1 to next
+//      return l1
+//  if 12's value is less than l1
+//      set l2's next to merge list while incrementing l2 to next
+//      return l2
+//
+// pseduo 2
+//
+//  create a dummy node
+//  create a tail node and set it equal to the dummy node
+//  while l1 and l2 is not null
+//      if l1's val is less than l2's val
+//          set the tail's next to be l1
+//          increment l1 by setting it to its next node
+//      if l2's val is less than l1's val
+//          set tails nexto tbe be l2
+//          increment l2 by setting l2 to its next node
+//      increment tail by setting it equal to next
+//  since l1 or l2 might not be null set tail.next to whichever is not null
+//  return dummy.next since the merge started at next
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return recursive(list1, list2);
+        // return iterate(list1, list2);
+    }
+    private ListNode recursive(ListNode l1, ListNode l2){
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+        if(l1.val < l2.val){
+            l1.next = recursive(l1.next, l2);
+            return l1;
+        }
+        else{
+            l2.next = recursive(l1, l2.next);
+            return l2;
+        }
+    }
+    private ListNode iterate(ListNode list1, ListNode list2){
+        ListNode root = new ListNode(-1);
+        ListNode dummy = root;
+        while(list1 != null && list2 !=null){
+            if(list1.val < list2.val){
+                dummy.next = list1;
+                list1 = list1.next;
             }
             else{
-                prev.next = l2;
-                l2 = l2.next;
+                dummy.next = list2;
+                list2 = list2.next;
             }
-            prev = prev.next;
+            dummy = dummy.next;
         }
-        prev.next = l1 == null ? l2 : l1;
-        return prehead.next;
+        dummy.next = list1 == null ? list2 : list1;
+        return root.next;
     }
 }
--- LinkedList/ReverseLinkedList/ReverseLinkedList.java
+++ LinkedList/ReverseLinkedList/ReverseLinkedList.java
@@ -18,30 +18,59 @@ class ListNode {
  * 
  */
 public class ReverseLinkedList {
+  
+}
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+//
+// pseduo code 1 iterative
+//
+//  create a prev node and set it to null
+//  while head is not null
+//      create a current node and point it to head
+//      increment head by setting head to next
+//      point currents next node to prev
+//      set prev to the curr node
+//  return prev
+
+// pseduo code 2 recursive
+//  (curr, prev)
+//      if curr is null return prev
+//      create a new node (temp) and set it to curr's next node
+//      set temps next to prev
+//      recurve using (temp, prev)
+class Solution {
     public ListNode reverseList(ListNode head) {
+        //return iterate(head);
+        return recurse(head, null);
+    }
+    private ListNode recurse(ListNode curr, ListNode prev){
+        if(curr == null){
+            return prev;
+        }
+        // create a pointer to the old curr next
+        ListNode temp = curr.next;
 
-        // stacks are first in last out
-        // so the last one put onto the stack
-        // will be the first one popped off
-        Stack<Integer> stack = new Stack<Integer>();
-        
-        // put all the values onto the stack
-        while(head!=null){
-            stack.push(head.val);
+        // set the new curr next to prev
+        curr.next = prev;
+
+        // recurse
+        return recurse(temp, curr);
+    }
+    private ListNode iterate(ListNode head){
+        ListNode prev = null;
+        while(head != null){
+            // keep a reference to head
+            ListNode curr = head;
+            
+            // increment head
             head = head.next;
-        }
 
-        // create a base node
-        ListNode out = new ListNode(-1);
+            // set the current node's nvext to the previous node
+            curr.next = prev;
 
-        ListNode n = out;
-        while(!stack.isEmpty()){
-            Integer v = stack.pop();
-            n.next = new ListNode(v);
-            // we need the links to be pointed in backwards order
-            n = n.next;
+            // incremnt prev (really a decrement)
+            prev = curr;
         }
-        // we dont want to return the -1 we instantiated
-        return out.next;
+        return prev;        
     }
 }
